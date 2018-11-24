9326dd413c6d30f18c7920a7289ec0a90653b6f2
Alexandr <sas88@inbox.ru>
2018-11-24 15:19:49 +0300

    первое дз

--- JS verstka/js/script.js
+++ JS verstka/js/script.js
@@ -1,1 +1,17 @@
-var first = 1;
+
+var budget = +prompt("Ваш бюджет?"); 
+var shopname = prompt("Название вашего магазина?");
+
+var mainList = {
+	budget,
+	shopname,
+	shopGoods: [],
+	employers: 0,
+	open: 0
+};
+
+for (var i = 0; i < 3; i++) {
+	mainList.shopGoods[i] = prompt("Какой тип товаров будем продавать?");
+}
+
+alert(mainList.budget / 30);
